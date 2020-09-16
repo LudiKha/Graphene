f0cd054d63e7f80bb27ba4e762496b3e3704ef0a
Khaya Ludidi <k.ludidi@kinstrife.com>
2020-09-16 16:20:42 +0200

    Improve template matching for primitive types

--- Core/Scripts/Templating/ComponentTemplates.cs
+++ Core/Scripts/Templating/ComponentTemplates.cs
@@ -4,8 +4,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Assertions;
+using UnityEngine.Events;
 using UnityEngine.UIElements;
+
+using Kinstrife.Core.ReflectionHelpers;
 
 namespace Graphene
 {
@@ -34,40 +36,43 @@ namespace Graphene
     [SerializeField] Dictionary<ControlType, Template> mapping = new Dictionary<ControlType, Template>();
     public IReadOnlyDictionary<ControlType, Template> Mapping => mapping;
 
-    // From generic object
-    public Template TryGetTemplate(object data, ControlType controlType)
-    {
-      return GetTemplateRecursive(controlType);
-    }
-
     public Template TryGetTemplate(object data, DrawAttribute drawAttribute = null)
     {
       ControlType controlType = ControlType.None;
 
-      // Get control type from class attribute
-      if (drawAttribute == null || drawAttribute.controlType == ControlType.None)
-        drawAttribute = Attribute.GetCustomAttribute(data.GetType(), typeof(DrawAttribute)) as DrawAttribute;
+      // No member draw attribute -> try get ControlType from class attribute
+      if ((drawAttribute == null || drawAttribute.controlType == ControlType.None) && !data.GetType().IsPrimitive && !(data is string))
+      {
+        var info = TypeInfoCache.GetExtendedTypeInfo(data.GetType());
+        info.HasTypeAttribute<DrawAttribute>();
+        drawAttribute = info.GetTypeAttribute<DrawAttribute>();
+      }
+      // Set ControlType from attribute (if present)
       if (drawAttribute != null)
         controlType = drawAttribute.controlType;
+
+      // Didn't find in attribute
+      if (controlType == ControlType.None)
+        controlType = GetControlTypeFromData(data);
 
       return GetTemplateRecursive(controlType);
     }
 
-
-    public Template TryGetTemplateForField<T>(T data)
+    public static ControlType GetControlTypeFromData(object data)
     {
-      ControlType controlType = ControlType.None;
-      if (data is float)
-        controlType = ControlType.Slider;
-      else if (data is bool)
-        controlType = ControlType.Toggle;
-      else if (data is System.Action || data is UnityEngine.Events.UnityEvent)
-        controlType = ControlType.Button;
+      if (data is bool)
+        return ControlType.Toggle;
+      else if (data is float)
+        return ControlType.Slider;
+      else if (data is int)
+        return ControlType.SliderInt;
       else if (data is string)
-        controlType = ControlType.Label;
-
-
-      return GetTemplateRecursive(controlType);
+        return ControlType.Label;
+      else if (data is System.Action || data is UnityEvent)
+        return ControlType.Button;
+      else if (data is IList<string>)
+        return ControlType.ListView;
+      return ControlType.None;
     }
 
     internal Template GetTemplateRecursive(ControlType controlType)
