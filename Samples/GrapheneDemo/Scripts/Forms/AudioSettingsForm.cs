2da8e48be25af00a798eb3e0093e51282c868faa
Khaya Ludidi <k.ludidi@kinstrife.com>
2020-09-16 16:23:10 +0200

    Cleaned up audio settings form

--- Samples/GrapheneDemo/Scripts/Forms/AudioSettingsForm.cs
+++ Samples/GrapheneDemo/Scripts/Forms/AudioSettingsForm.cs
@@ -11,38 +11,29 @@ namespace Graphene
   [CreateAssetMenu(menuName = "UI/Forms/AudioSettingsForm")]
   public class AudioSettingsForm : Form
   {
-    [Draw(ControlType.Label), Bind("Title")]
-    public string header = "AudioMixer";
+    [Draw, Bind]
+    public string VolumeHeader = "Volume";
 
-    [Draw] 
-    public BindableFloat MasterVolume;
-    [Draw]
-    public BindableFloat SFXVolume;
-    [Draw]
-    public BindableFloat MusicVolume;
+    [Draw, BindFloat("", 0.5f, 0,1, "Master Volume")] 
+    public float MasterVolume = 0.5f;
+
+    [Draw, BindFloat("", 0.5f, 0, 1, "SFX Volume")]
+    public float SFXVolume;
+    [Draw, BindFloat("", 0.5f, 0, 1, "Music Volume")]
+    public float MusicVolume;
 
     [Draw(ControlType.Label), Bind("Info")]
     public string info = "Other";
 
-    [Draw(ControlType.Label)]
+    [Draw(ControlType.TextField)]
     public BindableBaseField<string> infoBindable = new BindableBaseField<string> { Value = "Information", Label = "Informaçione" };
 
-    [Draw, BindFloat("Value", 1, 0, 1, "StereoBind", true)]
-    public BindableFloat Stereo;
-
-    [Draw(controlType = ControlType.Slider), BindFloat("Value", 1, 0, 3, "Volume1234", true)]
-    public float GeneralVolume = 5;
-
-
-    [Draw(ControlType.CycleField), BindBaseField("Value", "Named Int Path", true)]
+    [Draw(ControlType.CycleField), BindBaseField("", "Named Int Path", true)]
     public BindableNamedInt Mode;
 
     [Draw(ControlType.TextField), BindString("Value", "Input 'ere", label: "Dat label tho")]
     public string twoWayText = "TwoWayText";
 
-
-    [Draw(ControlType.Toggle), BindBaseField("Value", "Da label")]
-    public bool toggle = true;
 
     public override void InitModel()
     {
