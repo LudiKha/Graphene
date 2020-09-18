d39cc83031136348346bcb94cf4eb66645b1c6f4
Khaya Ludidi <k.ludidi@kinstrife.com>
2020-09-18 16:32:19 +0200

    Added EditorUtilities

---
+++ src/Editor/GrapheneEditorUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Graphene
+{
+  public static class GrapheneEditorUtilities
+  {
+    public const string uuid = "com.cupbearer.graphene";
+
+    [MenuItem("Window/Graphene/Update to latest version")]
+    static void UpdateToLatest()
+    {
+      UnityEditor.PackageManager.Client.Add(uuid);
+    }
+  }
+}
