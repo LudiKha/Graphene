937b8676104b15e110a3a597e33eba40decab85d
Khaya Ludidi <k.ludidi@kinstrife.com>
2020-09-29 00:23:10 +0200

    Added EnableOnState component to enable/disable certain plates based on route

---
+++ src/Core/Scripts/Routing/EnableOnState.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Graphene
+{
+  [RequireComponent(typeof(Plate))]
+  [DisallowMultipleComponent]
+  public abstract class EnableOnState<T> : MonoBehaviour, IInjectable, IInitializable
+  {
+    public enum ActivationMode
+    {
+      EnableOnStates,
+      DisableOnStates
+    }
+    [SerializeField] ActivationMode mode;
+
+    [SerializeField] protected Plate plate;
+    [SerializeField] List<T> states = new List<T>();
+    protected Router<T> router;
+
+    public bool Initialized { get; private set; }
+    public virtual void Initialize()
+    {
+      if (Initialized)
+        return;
+      Initialized = true;
+
+      if (plate || (plate = GetComponent<Plate>()))
+        plate.onEvaluateState += Plate_onEvaluateState;
+
+      // Get the router in case we didn't inject
+      if (!router)
+        router = GetComponentInParent<Router<T>>();
+
+      // Subscribe to router state changes
+      router.onStateChange += Router_onStateChange;
+    }
+
+    /// <summary>
+    /// Dependency injection handle
+    /// </summary>
+    /// <param name="router"></param>
+    public void Inject(Router<T> router)
+    {
+      this.router = router;
+    }
+
+
+    private void Router_onStateChange(T address)
+    {
+      bool match = states.IndexOf(router.LeafStateFromAddress(address)) >= 0;
+      bool show = false;
+      switch (mode)
+      {
+        case ActivationMode.EnableOnStates:
+          show = match;
+          break;
+        case ActivationMode.DisableOnStates:
+          show = !match;
+          break;
+        default:
+          break;
+      }
+
+      if (show)
+        plate.Show();
+      else
+        plate.Hide();
+    }
+
+    protected void Plate_onEvaluateState()
+    {
+      Router_onStateChange(router.CurrentState);
+    }
+  }
+
+}
